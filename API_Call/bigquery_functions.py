0fef92e9370a9c3bd3cea1fa3578cb6025ce7daa
Tanasorn Chindasook <m.chindasook@exporo.com>
2020-07-22 22:42:33 +0200

    api call fixes

--- API_Call/bigquery_functions.py
+++ API_Call/bigquery_functions.py
@@ -15,32 +15,11 @@ def insert_rows_to_tables(credentials,
     insert_rows has to be a list of tuples.
     '''
     # Instantiates a client
-    bigquery_client = bigquery.Client(credentials= credentials,project=project_id)
+    client = bigquery.Client(credentials= credentials,project=project_id)
 
     query = f"""
-            INSERT INTO {DATASET_ID}.{TABLE_ID}
+            INSERT INTO {dataset_id}.{table_id}
             VALUES {insert_rows}
             """
 
     query_job = client.query(query)
-
-def insert_rows_to_tables_new(credentials,
-                          project_id,
-                          dataset_id,
-                          table_id,
-                          insert_rows):
-    '''
-    insert_rows has to be a list of tuples.
-    '''
-    # Instantiates a client
-    bigquery_client = bigquery.Client(credentials= credentials,project=project_id)
-
-    # Prepares a reference to the dataset
-    dataset_ref = bigquery_client.dataset(dataset_id)
-
-    table_ref = dataset_ref.table(table_id)
-    table = bigquery_client.get_table(table_ref)  # API call
-
-    rows_to_insert = insert_rows
-    errors = bigquery_client.insert_rows(table, rows_to_insert)  # API request
-    assert errors == []
